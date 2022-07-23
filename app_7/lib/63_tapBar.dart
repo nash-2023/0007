bfa1e77e637a1bddd859866cfda425ec72be1121
home_lab <abdalla.fat7y@hotmail.com>
2022-07-23 02:11:48 +0200

    tap bar home lab

---
+++ app_7/lib/63_tapBar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class tTapBarView extends StatefulWidget {
+  const tTapBarView({Key? key}) : super(key: key);
+
+  @override
+  State<tTapBarView> createState() => _tTapBarViewState();
+}
+
+class _tTapBarViewState extends State<tTapBarView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
